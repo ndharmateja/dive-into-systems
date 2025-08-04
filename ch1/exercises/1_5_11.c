ef725dd2599d4c1ebaadb2353387bd767257a7c3
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-08-04 08:58:28 +0530

    convert while to do-while and better print formatting

--- ch1/exercises/1_5_11.c
+++ ch1/exercises/1_5_11.c
@@ -5,13 +5,14 @@
 
 void string_copy(char destination[], char source[])
 {
+    // We can use the do-while here because we need to copy the
+    // null terminator as well
     int i = 0;
-    while (source[i] != '\0')
+    do
     {
         destination[i] = source[i];
         i++;
-    }
-    destination[i] = '\0';
+    } while (source[i] != '\0');
 }
 
 int main(void)
@@ -21,21 +22,21 @@ int main(void)
     // Test Case 1: Copy a normal string.
     printf("Case 1\n");
     strcpy(dest, "Hello, World!");
-    printf("strcpy output: '%s'\n", dest);
+    printf("strcpy output.    : '%s'\n", dest);
     string_copy(dest, "Hello, World!");
     printf("string_copy output: '%s'\n\n", dest);
 
     // Test Case 2: Copy an empty string.
     printf("Case 2\n");
     strcpy(dest, "");
-    printf("strcpy output: '%s'\n", dest);
+    printf("strcpy output.    : '%s'\n", dest);
     string_copy(dest, "");
     printf("string_copy output: '%s'\n\n", dest);
 
     // Test Case 3: Copy a single character string.
     printf("Case 3\n");
     strcpy(dest, "A");
-    printf("strcpy output: '%s'\n", dest);
+    printf("strcpy output.    : '%s'\n", dest);
     string_copy(dest, "A");
     printf("string_copy output: '%s'\n\n", dest);
 
@@ -43,7 +44,7 @@ int main(void)
     char dest4[11];
     printf("Case 4\n");
     strcpy(dest4, "1234567890");
-    printf("strcpy output: '%s'\n", dest4);
+    printf("strcpy output.    : '%s'\n", dest4);
     string_copy(dest4, "1234567890");
     printf("string_copy output: '%s'\n\n", dest4);
 
@@ -55,14 +56,14 @@ int main(void)
 
     printf("Case 5\n");
     strcpy(long_dest, long_src);
-    printf("strcpy output: '%s'\n", long_dest);
+    printf("strcpy output.    : '%s'\n", long_dest);
     string_copy(long_dest, long_src);
     printf("string_copy output: '%s'\n\n", long_dest);
 
     // Test Case 6: Copy a string with special characters.
     printf("Case 6\n");
     strcpy(dest, "Hello\nWorld\t!");
-    printf("strcpy output: '%s'\n", dest);
+    printf("strcpy output.    : '%s'\n", dest);
     string_copy(dest, "Hello\nWorld\t!");
     printf("string_copy output: '%s'\n\n", dest);
 
